7c8a5555bdf1ae81f4ee97ba49af6d4bfd5785ff
FekeHunter <likun@51mydao.com>
2020-05-19 16:20:18 +0800

    修改为直接上传目录

--- index.js
+++ index.js
@@ -1,124 +1,71 @@
-const fs = require("fs");
-const node_ssh = require("node-ssh");
-const defaultConfig = require("./default.config.json");
-const childProcess = require("child_process");
-const process = require("process");
-const archiver = require("archiver");
+const node_ssh = require('node-ssh')
+const defaultConfig = require('./default.config.json')
+const childProcess = require('child_process')
+const process = require('process')
 
-const outConfig = require("../../deploy.config.json");
-const projectDir = process.cwd();
+const outConfig = require('../../deploy.config.json')
+const projectDir = process.cwd()
 
-let config = mergeConfig(outConfig, defaultConfig);
-console.log(projectDir);
+let config = mergeConfig(outConfig, defaultConfig)
+console.log(projectDir)
 
-init(config);
+init(config)
 
 function mergeConfig(outConfig, defaultConfig) {
-  console.log("混合配置...");
+  console.log('混合配置...')
 
-  let _config = outConfig
-    ? Object.assign(defaultConfig, outConfig)
-    : defaultConfig;
+  let _config = outConfig ? Object.assign(defaultConfig, outConfig) : defaultConfig
 
-  return _config;
+  return _config
 }
 
 async function init(config) {
-  let { build } = config;
-  await execBuild(build.script);
+  let { build } = config
+  await execBuild(build.script)
 
-  await makeZip(config);
+  await connect(config)
 
-  await connect(config);
-
-  deleteZip(config.remote.dirName);
-
-  process.exit(0);
+  process.exit(0)
 }
 
 function execBuild(script) {
-  console.log("打包开始，工作目录：", projectDir);
+  console.log('打包开始，工作目录：', projectDir)
   return new Promise((rel, rej) => {
-    childProcess.exec(
-      script,
-      { cwd: projectDir, maxBuffer: 100000 * 1024, timeout: 100000 },
-      function(err) {
-        if (err) {
-          rej(err);
-          return;
-        }
-        rel();
-        console.log("打包成功");
+    childProcess.exec(script, { cwd: projectDir, maxBuffer: 1000000 * 1024, timeout: 100000 }, function(err) {
+      if (err) {
+        rej(err)
+        return
       }
-    );
+      rel()
+      console.log('打包成功')
+    })
   }).catch(err => {
-    console.log("打包失败");
-    console.log(err);
-    process.exit(1);
-  });
-}
-
-function makeZip(config) {
-  let { build, remote } = config;
-  const archive = archiver("zip", {
-    zlib: { level: 9 }
-  }).on("error", err => {
-    throw err;
-  });
-
-  // 创建文件输出流
-  const output = fs.createWriteStream(projectDir + `/${remote.dirName}.zip`);
-
-  // 通过管道方法将输出流存档到文件
-  archive.pipe(output);
-
-  // 从subdir子目录追加内容并重命名
-  archive.directory(build.pathName, remote.dirName);
-
-  // 完成打包归档
-  archive.finalize();
-  console.log("完成压缩");
-}
-
-function deleteZip(dirName) {
-  childProcess.execSync(`del ${projectDir}\\${dirName}.zip`, {
-    cwd: projectDir
-  });
+    console.log('打包失败')
+    console.log(err)
+    process.exit(1)
+  })
 }
 
 async function connect(config) {
-  const ssh = new node_ssh();
-  const { path, dirName } = config.remote;
-  const zipFileName = `${dirName}.zip`;
-  const remotePath = path;
+  const ssh = new node_ssh()
+  const { path, dirName } = config.remote
+  const remotePath = path
 
   try {
-    await ssh.connect(config.servers);
-    console.log("链接成功!");
+    await ssh.connect(config.servers)
+    console.log('链接成功!')
 
-    let path = projectDir + `\\${zipFileName}`;
+    console.log('删除远程目录成功')
+    await ssh.execCommand(`rm -rf ${dirName}`, { cwd: remotePath })
 
-    console.log("上传路径" + path);
-    console.log("远程路径" + remotePath);
+    console.log('远程路径' + remotePath)
 
-    await ssh.putFile(path, remotePath + zipFileName);
+    await ssh.putDirectory(projectDir + '\\' + config.build.pathName, remotePath + dirName)
 
-    console.log("文件上传成功");
-
-    await ssh.execCommand(`rm -rf ${dirName}`, { cwd: remotePath });
-
-    console.log("删除远程目录成功");
-
-    await ssh.execCommand(`unzip ${zipFileName}`, { cwd: remotePath });
-
-    console.log("文件解压成功");
-
-    await ssh.execCommand(`rm -f ${zipFileName}`, { cwd: remotePath });
-
-    console.log("删除远程文件");
+    console.log('文件上传成功')
   } catch (error) {
-    console.error("上传失败", ":", error);
-    deleteZip(dirName);
-    process.exit(1);
+    console.error('上传失败', ':', error)
+
+    process.exit(1)
   }
 }
