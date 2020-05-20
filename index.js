81cbb279d2cb620e5ba268a439d7220596ae3f45
FekeHunter <likun@51mydao.com>
2020-05-20 17:48:17 +0800

    fix bug

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ const runCommand = require('./src/runCommand')
 const config = require('./src/getConfig')
 const compress = require('./src/compressed')
 const connectAndUpload = require('./src/connect')
-const {Log} = require('./src/utils')
+const { Log } = require('./src/utils')
 const delectLoaclZip = require('./src/delectLoaclZip')
 
-async function init () {
+async function init() {
   try {
     // 用户打包
     await runCommand(config.local.script)
-    Log('打包完成') 
+    Log('打包完成')
 
     await compress(config)
-    Log('压缩完成') 
+    Log('压缩完成')
 
     await connectAndUpload(config)
 
     await delectLoaclZip(config)
     Log('删除本地文件')
+    process.exit()
   } catch (error) {
-    delectLoaclZip(config)
+    delectLoaclZip(config).then(() => {
+      process.exit(1)
+    })
   }
 }
 
-init () 
+init()
